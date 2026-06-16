244a15630597b4c74dddfe0c05c0e3a386851ed7
Farnoosh <farnooshsaadati66@gmail.com>
2026-06-16 12:11:16 +0200

    Improve plotting: add bar chart, title, axis labels and colors

--- mergesort.py
+++ mergesort.py
@@ -1,51 +1,69 @@
-def ASSIGNMENT(new_list, i, old_list, j):
-    new_list[i] = old_list[j]
+def copy_element(target, target_idx, source, source_idx):
+    """Copy one element from source list into target list."""
+    target[target_idx] = source[source_idx]
 
 
-def mergeSort(list_to_sort_by_merge):
-    if (
-        len(list_to_sort_by_merge) > 1
-        and not len(list_to_sort_by_merge) < 1
-        and len(list_to_sort_by_merge) != 0
-    ):
-        mid = len(list_to_sort_by_merge) // 2
-        left = list_to_sort_by_merge[:mid]
-        right = list_to_sort_by_merge[mid:]
+def merge_sort(arr):
+    """
+    Sort a list in ascending order using the merge sort algorithm.
 
-        mergeSort(left)
-        mergeSort(right)
+    Merge sort works by:
+    1. Splitting the list in half
+    2. Recursively sorting each half
+    3. Merging the two sorted halves back together
+    """
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
 
-        l = 0
-        r = 0
-        i = 0
+        merge_sort(left)
+        merge_sort(right)
 
-        while l < len(left) and r < len(right):
-            if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
-                l += 1
+        left_idx = 0
+        right_idx = 0
+        merged_idx = 0
+
+        while left_idx < len(left) and right_idx < len(right):
+            if left[left_idx] <= right[right_idx]:
+                copy_element(arr, merged_idx, left, left_idx)
+                left_idx += 1
             else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
-                r += 1
-            i += 1
+                copy_element(arr, merged_idx, right, right_idx)
+                right_idx += 1
+            merged_idx += 1
 
-        while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
-            l += 1
-            i += 1
+        while left_idx < len(left):
+            arr[merged_idx] = left[left_idx]
+            left_idx += 1
+            merged_idx += 1
 
-        while r < len(right):
-            list_to_sort_by_merge[i] = right[r]
-            r += 1
-            i += 1
+        while right_idx < len(right):
+            arr[merged_idx] = right[right_idx]
+            right_idx += 1
+            merged_idx += 1
 
 
 import matplotlib.pyplot as plt
 
 my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-x = range(len(my_list))
-plt.plot(x, my_list)
+
+# Plot before sorting
+plt.figure(figsize=(8, 4))
+plt.bar(range(len(my_list)), my_list, color='steelblue')
+plt.title("List Before Sorting")
+plt.xlabel("Index")
+plt.ylabel("Value")
+plt.tight_layout()
 plt.show()
-mergeSort(my_list)
-x = range(len(my_list))
-plt.plot(x, my_list)
+
+merge_sort(my_list)
+
+# Plot after sorting
+plt.figure(figsize=(8, 4))
+plt.bar(range(len(my_list)), my_list, color='green')
+plt.title("List After Sorting")
+plt.xlabel("Index")
+plt.ylabel("Value")
+plt.tight_layout()
 plt.show()
